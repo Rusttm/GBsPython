45f2ae79e234ab6083b0a8361bc25ef4a273499a
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-01-04 19:29:33 +0300

    dz4 commit

--- 2 part/dz4.py
+++ 2 part/dz4.py
@@ -2,10 +2,18 @@
 # Найдите произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
-
+import random
 
 n = int(input('Enter the Number '))
+pos1 = int(input('Enter the 1st position '))
+pos2 = int(input('Enter the 2nd position '))
 
 if (n<0): n = -n
 my_array = list(range(-n,n+1))
-print(my_array)
+random_list = []
+for i in range(2*n+1):
+    elem = my_array.pop(random.randint(0,2*n-i))
+    random_list.append(elem)
+
+print(f'Для N = {n}: {random_list}')
+print(f'Произведение элементов {pos1} и {pos2} = {random_list[pos1]*random_list[pos2]}')
