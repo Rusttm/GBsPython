50645c5c748c375207695c8802390f9f1945ef51
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-01-04 19:36:23 +0300

    dz5 commit

--- 2 part/dz5.py
+++ 2 part/dz5.py
@@ -1,4 +1,13 @@
 # Реализуйте алгоритм перемешивания списка.
 
 
+import random
 
+n = int(input('Задайте количество элементов в списке: '))
+my_array = list(range(1,n+1))
+print(f' Для массива {my_array}', end=' -> ')
+random_list = []
+for i in range(n):
+    elem = my_array.pop(random.randint(0,n-i-1))
+    random_list.append(elem)
+print(random_list)
