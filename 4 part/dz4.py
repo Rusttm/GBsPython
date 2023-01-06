e8924fca27614d2f52574c7e0101d778211b5dac
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-01-06 21:11:33 +0300

    dz4 commit

---
+++ 4 part/dz4.py
@@ -0,0 +1,40 @@
+# 4 Задана натуральная степень k. Сформировать случайным образом 
+# список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+# *Пример:* 
+
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+k = int(input('Введите степень уравнения:'))
+from sympy import *
+import random
+
+coef_list = []
+for i in range(k+1):
+    coef = random.randint(-100,101)
+    coef_list.append(coef)
+
+
+
+x_list = [str(f'x**{i}') if i!=1 else str(f'x') for i in range(k,0,-1)]
+
+result_string = ''
+
+for i in range(k):
+    if (coef_list[i] != 0):
+        result_string += f'{coef_list[i]}*{x_list[i]}'
+        if coef_list[i+1] > 0:
+            result_string += '+'
+result_string += f'{coef_list[k]} = 0'
+
+
+with open('dz4_file.txt', 'w') as f:
+    f.write(result_string)
+
+
+print(result_string)
+
+
+# x = Symbol('x')
+# expr = x**2 + x + 5
+# print(expr)
