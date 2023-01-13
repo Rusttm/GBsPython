b8969790d95838dd50ac5b519c26f6d2d6498ca1
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-01-13 21:20:15 +0300

    dz4 code and write

--- 5 part/dz4.py
+++ 5 part/dz4.py
@@ -1,2 +1,29 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
+file_4code = '5dz3_rle_4code.txt'
+file_coded = '5dz3_rle_coded.txt'
+file_decoded = '5dz3_rle_decoded.txt'
+
+my_textes = []
+
+with open(file_4code, 'r') as f1:
+    my_textes = f1.readlines()
+
+print(my_textes)
+
+def LineCoder(my_string: str) -> str:
+    result_line = ''
+    current_char = my_string[0]
+    counter = 0
+    for i in range(len(my_string)):
+        if current_char == my_string[i]:
+            counter += 1
+        else:
+            result_line += f'{counter}{my_string[i-1]}'
+            counter = 1
+        current_char = my_string[i]
+    return result_line
+
+with open(file_coded, 'w') as f2:
+    for elem in my_textes:
+        f2.write(f'{LineCoder(elem)}\n')
