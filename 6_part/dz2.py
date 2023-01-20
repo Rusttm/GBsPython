b39e07a3d0254c721b1770d0cf44dd8da51bed67
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-01-20 12:41:09 +0300

    7part initial commit

--- 6_part/dz2.py
+++ 6_part/dz2.py
@@ -1,2 +1,8 @@
 # 2 Задайте натуральное число N. 
 # Напишите программу, которая составит список простых множителей числа N.
+
+n = int(input('Enter number: '))
+
+print(list(map(lambda x: [y for y in range(1,x+1)], filter(lambda m: n%m==0, range(1,n+1)) )))
+
+# print(list(filter(lambda x: x,[True, True, False, True])))
