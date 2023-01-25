cf6fc06c8ca592290daf3949e49c630073aceddf
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-01-25 15:48:32 +0300

    data transfer from csv

---
+++ 8_part/DataTransfer.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+# import sklearn
+# print(sklearn.__version__)
+
+
+def DataFromTitanic(user = 'user', n = 100):
+    data_csv = '/Users/johnlennon/Downloads/titanic.csv'
+    data = pd.read_csv(data_csv)
+    user_data, manager_data = train_test_split(data, test_size=0.2, random_state=77)
+    result = []
+    if user == 'user':
+        for i in range(n):
+            result.append(user_data['Name'].iloc[i])
+    elif user == 'manager':
+        for i in range(n):
+            result.append(manager_data['Name'].ilo[i])
+    else:
+        return []
+    return result
+
+
+if '__name__' == '__main__':
+    print(DataFromTitanic(user='user', n = 10))
