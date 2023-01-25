32ed5f30e3aa47c1763b43d8f6c5c1567fd12aa6
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-01-25 17:30:39 +0300

    tables filled

--- 8_part/DataTransfer.py
+++ 8_part/DataTransfer.py
@@ -4,17 +4,19 @@ from sklearn.model_selection import train_test_split
 # print(sklearn.__version__)
 
 
-def DataFromTitanic(user='user', n=100):
+def DataFromTitanic(user='user', n=False):
     data_csv = '/Users/johnlennon/Downloads/titanic.csv'
     data = pd.read_csv(data_csv)
     user_data, manager_data = train_test_split(data, test_size=0.2, random_state=77)
     result = []
+    if not n:
+        n = 10
     if user == 'user':
         for i in range(n):
             result.append(user_data['Name'].iloc[i])
     elif user == 'manager':
         for i in range(n):
-            result.append(manager_data['Name'].ilo[i])
+            result.append(manager_data['Name'].iloc[i])
     else:
         return []
     return result
--- 8_part/MysqlfillModule.py
+++ 8_part/MysqlfillModule.py
@@ -1,4 +1,6 @@
 import mysql.connector
+import DataTransfer
+import random
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -8,6 +10,51 @@ mydb = mysql.connector.connect(
     database='myGBdatabase')
 
 mycursor = mydb.cursor()
+def FillDataManagers():
+    rooms_list = GetColFromTbl(tbl_name='Office', column='room',  condition="floor=4")
+    managers_list = DataTransfer.DataFromTitanic(user='manager', n=15)
+    for manager in managers_list:
+        room = rooms_list[random.randint(1, 14)]
+        print(f"('{manager}, {room}' )")
+        FillTblData(tbl_name='Managers', tbl_scheme='(name, room)', data=f"('{manager}', '{room}' )")
+    # mydb.commit()
+
+def FillDataDepartments():
+    dep_list = ['Sales', 'Supply', 'Transport', 'Storage', 'Purchases', 'Tech', 'Finance']
+    office_dict = {'Podolsk': ['Supply', 'Storage'], 'Moscow1': ['Sales', 'Transport', 'Purchases', 'Tech', 'Finance']}
+    managers_ids_list = GetColFromTbl(tbl_name='Managers', column='manager_id',  condition=False)
+
+    for address, deps in office_dict.items():
+        for dep in deps:
+            manager_id = random.choice(managers_ids_list)[0]
+            print(f"('{manager_id}', '{dep}, '{address}')")
+            FillTblData(tbl_name='Departments', tbl_scheme='(manager_id, name, address)', data=f"('{manager_id}', '{dep}', '{address}')")
+    # mydb.commit()
+
+def FillDataEmployees():
+    rooms_list = GetColFromTbl(tbl_name='Office', column='room,floor',  condition="floor<4")
+    users_list = DataTransfer.DataFromTitanic(user='user', n=500)
+    managers_ids_list = GetColFromTbl(tbl_name='Managers', column='manager_id', condition=False)
+    deps_list = GetColFromTbl(tbl_name='Departments', column='department_id, address', condition=False)
+
+    for user in users_list:
+        user = user.replace("'", "")
+
+        manager_id = random.choice(managers_ids_list)[0]
+        dep = random.choice(deps_list)
+        dep_id = dep[0]
+        address = dep[1]
+        room = random.choice(rooms_list)
+        room_no = room[0]
+        phone = random.randint(1000000, 10000000)
+        print(f"('{user=}, {manager_id=}, {dep_id=}, {phone=}, {address=}, {room_no=}')")
+        FillTblData(tbl_name='Employees',
+                    tbl_scheme='(name, manager_id, department_id, phone, address, room)',
+                    data=f"('{user}', '{manager_id}', '{dep_id}', '{phone}', '{address}', '{room_no}')")
+        # break
+    # mydb.commit()
+
+
 def FillDataOffice():
     office_address = '119002, Moscow, per Denezhniy, 19'
     for floor in range(1, 5):
@@ -15,6 +62,8 @@ def FillDataOffice():
             room = f'{floor}{ii}'
             FillTblData(tbl_name='Office', tbl_scheme='(room, floor, address)', data=f"('{room}', '{floor}', '{office_address}')")
     # mydb.commit()
+
+
 
 def FillTblData(tbl_name = 'Office', tbl_scheme = '(room, floor, address)', data = "('1','2','test')"):
     mycursor.execute(f"INSERT INTO {tbl_name} {tbl_scheme} VALUES {data}")
@@ -35,12 +84,16 @@ def GetColFromTbl(tbl_name = 'Office', column ='room',  condition = False):
         mycursor.execute(f"SELECT {column} FROM {tbl_name} WHERE {condition}")
     my_result = mycursor.fetchall()
     for x in my_result:
-        result.append(x[0])
+        result.append(list(x))
     return result
 
 def FullFillAll():
     # ClearTblData(tbl_name = 'Office')
     # FillDataOffice()
     # print(GetColFromTbl(tbl_name = 'Office', column ='room',  condition = "floor=4"))
+    # FillDataManagers()
+    # FillDataDepartments()
+    # FillDataEmployees()
 
-FullFillAll()
+if '__name__' == '__main__':
+    FullFillAll()
