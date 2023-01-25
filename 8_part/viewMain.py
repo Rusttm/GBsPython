d30bf5d5c0dac1072b54415f626e289b6110d561
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-01-25 22:01:43 +0300

    fin commit

--- 8_part/viewMain.py
+++ 8_part/viewMain.py
@@ -1,3 +1,13 @@
+"""
+программа имеет GUI интерфейс для внесения и просмотра записей в базу сотрудников
+при этом запись и чтение осуществляются в модуле MysqlconnectionModule.py
+база данных развернута на Docker образе и начально заполняется с помощью файлов
+MysqlinitModule - инициализация
+MysqlfillModule - заполнение
+отдельным модулем создается окно Department
+"""
+
+
 import tkinter as tk
 import viewDep
 import MysqlconnectionModule
