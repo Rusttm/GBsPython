4dfbd3b3aa398cbcbddbe429de1873709edb9724
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-01-28 02:21:55 +0300

    try to use gamer logging commit

--- 9_part/bot_test.py
+++ 9_part/bot_test.py
@@ -36,45 +36,84 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+class JohnBotGB():
+    def __init__(self):
+        self.project = False
+        self.current_gamer = dict() # 'user_id' : 'tictac', calc or false
+        self.delete_msg_id = 0
+        self.game_dict = dict({'noname': {'function': self.start, 'help': 'Игра не выбрана'},
+                               'tictaс': {'function': self.GameTicTac, 'help': 'Введите координату'},
+                               'calc': {'function': self.GameCalc, 'help': 'Введите выражение'}})
 
-# Define a few command handlers. These usually take the two arguments update and
+        self.commands_dict = dict({'/start': '-перезапуск бота',
+                                   '/help': '- помощь',
+                                   '/tictac': '-игра в крестики-нолики',
+                                   '/calc': 'решение математического выражения'})
+        self.NewBot()
+
+    # Define a few command handlers. These usually take the two arguments update and
 # context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
+
+    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Send a message and restart bot when the command /start is issued."""
+        self.current_gamer[update.message.from_user.id] = 'noname'
+        user = update.effective_user
+        await update.message.reply_html(
+            rf"Hi {user.mention_html()}!",
+            reply_markup=ForceReply(selective=True),
+        )
+        msg = '\n'.join([str(f"{key}: {value}") for key, value in self.commands_dict.items()])
+        await update.message.reply_html(f"Основные функции:\n {msg}", reply_markup=ForceReply(selective=True))
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+    async def help_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Send a message when the command /help is issued."""
+
+        temp_msg = self.game_dict.get(self.current_gamer[update.message.from_user.id], self.game_dict['noname'])['help']
+        await update.message.delete()
+        await update.message.reply_text(f"{temp_msg}")
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    logging.info(msg=f"message from: {update.message.chat.first_name}, text: {update.message.text} ")
-    await update.message.reply_text(f"Hi {update.message.from_user.first_name}({update.message.from_user.id}) "
-                                    f"and you wrote {update.message.text}")
+    async def echo(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Echo the user message."""
+        logging.info(msg=f"message from: {update.message.chat.first_name}, text: {update.message.text} ")
+        await update.message.reply_text(f"Hi {update.message.from_user.first_name}({update.message.from_user.id}) "
+                                        f"and you wrote {update.message.text}")
+    async def GameTicTac(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        """start tictok game"""
+        self.current_gamer[update.message.from_user.id] = 'tictaс'
+        await update.message.reply_text("Игра крестики нолики началась -Ваш ход")
+
+        await update.message.reply_text("Игра крестики нолики закончена. Досвидания!")
+
+    async def GameCalc(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        """start tictok game"""
+        self.current_gamer[update.message.from_user.id] = 'calc'
+        await update.message.reply_text("Введите выражение для вычисления")
 
 
-def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token(TOKEN).build()
 
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling()
+    def NewBot(self) -> None:
+        """Start the bot."""
+
+        # Create the Application and pass it your bot's token.
+        application = Application.builder().token(TOKEN).build()
+
+        # on different commands - answer in Telegram
+        application.add_handler(CommandHandler("start", self.start))
+        application.add_handler(CommandHandler("help", self.help_command))
+        application.add_handler(CommandHandler("tictac", self.GameTicTac))
+        application.add_handler(CommandHandler("calc", self.GameCalc))
+        # on non command i.e message - echo the message on Telegram
+        application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.echo))
+
+        # Run the bot until the user presses Ctrl-C
+        application.run_polling()
+
+
 
 
 if __name__ == "__main__":
-    main()
+    bot_run = JohnBotGB()
