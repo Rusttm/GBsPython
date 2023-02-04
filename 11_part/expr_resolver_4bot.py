7405aff0478b156b681609142be23e77f6c868ba
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-02-04 15:38:22 +0300

    issues with linux resolved

--- 11_part/expr_resolver_4bot.py
+++ 11_part/expr_resolver_4bot.py
@@ -370,6 +370,7 @@ class ExpressionResolver:
                 for plot2 in graph_f_diff:
                     pts2 = plot2.get_points()
                     plt.plot(pts2[0], pts2[1])
+                plt.title(f'Function: \n{self.corrected_expr}\n Derivative: \n{self.dif_expr}')
                 plt.savefig(f'{user_name}_diff.png')
 
 
